8f1f523ba2a2a882a4135c8faab2dcca529d24e3
Shankar <m.panchalingam@stud.uis.no>
2022-09-27 15:09:28 +0200

    updates

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from typing import Union
 import uvicorn
 from fastapi import FastAPI
+import pickle
+import xgboost as xgb
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import explained_variance_score, mean_absolute_error, r2_score, mean_absolute_percentage_error, \
+    mean_squared_error
 
 app = FastAPI()
+
+# load
+model = xgb.XGBRegressor()
+model.load_model('xgb_reg.txt')
 
 
 @app.get("/")
@@ -17,8 +27,23 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.get("/predict")
 def predict():
+    # read dataset
+    dataset = pd.read_csv("dataset_cleaned_V3.csv", index_col='DATETIME')
+
+    # split data into X and y
+    X = dataset.copy()
+    y = X.pop('PWEA-T')
+
+    # split data into train and test set with test size 0.2
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    predictions = model.predict(X_test)
+    metrics = {
+        'MSE score': mean_squared_error(y_test, predictions),
+        'R2 score': r2_score(y_test, predictions)
+    }
     return {
-        "Prediction": "Complete"
+        "Predictions": predictions.tolist(),
+        "Metrics": metrics
     }
 
 
